833395e348fab531a93e08ed906dca6c6bd44be3
omorihayato <hayato.xxx.8810@gmail.com>
2021-02-20 14:22:15 +0900

    serializer完成

--- api/serializers.py
+++ api/serializers.py
@@ -6,6 +6,7 @@ from .models import Post, Profile, Comment
 class UserSirializer(serializers.ModelSerializer):
 
     class Meta:
+        # カスタムしたuserモデルを取ってきてる
         model = get_user_model()
         fields = ('id', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
@@ -17,9 +18,29 @@ class UserSirializer(serializers.ModelSerializer):
 
 class ProfileSerializer(serializers.ModelSerializer):
 
+    # datetimeをパースする
     created_on = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
 
     class Meta:
         model = Profile
         fields = ('id', 'nickName', 'userProfile', 'created_on', 'img')
         extra_kwargs = {'userProfile': {'read_only': True}}
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    # datetimeをパースする
+    created_on = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'userPost', 'created_on', 'img', 'liked')
+        extra_kwargs = {'userPost': {'read_only': True}}
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'userComment', 'post')
+        extra_kwargs = {'userComment': {'read_only': True}}
